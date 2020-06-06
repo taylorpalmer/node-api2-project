f661411ab3db0546c5493c25231413247691694a
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-06-05 19:23:29 -0700

    setup, npm i

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const welcomeRouter = require("./welcome/welcome-router");
+const usersRouter = require("./users/users-router");
+
+const server = express();
+const port = 4000;
+
+server.use(express.json());
+
+// bring in our subrouters and attach them to the main application
+server.use(welcomeRouter);
+server.use(usersRouter);
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
